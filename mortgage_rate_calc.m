0de540fd1203c0fc850d18399180e3a2d3b01f4d
nkrish82 <narayanan.krishnann@gmail.com>
2015-04-16 20:27:03 -0700

    Update mortgage_rate_calc.m
    comment error

--- mortgage_rate_calc.m
+++ mortgage_rate_calc.m
@@ -7,7 +7,7 @@ t= 6; % loan period
 P = 30000; %principal amount
 
 %%%%Mortgage rate per month
-%%%% solving P(t-1)*exp(i) - 12*M = 0, P(n) = P(n-1) - 12*M for n = 0:1:29
+%%%% solving P(t-1)*exp(i) - 12*M = 0, P(n) = P(n-1) - 12*M for n = 0:1:t
 M = P*exp(t*i)/(sum (exp(i*(0:1:t)))*12);
 
 T = 12*M*t; % total amount owed
